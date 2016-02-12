4edef4acb8ef7b86cd5f3c590636c8943c97eaab
Jan Geselle <geselle.jan@googlemail.com>
2016-02-12 15:54:46 +0100

    bugfix

--- single-photos.php
+++ single-photos.php
@@ -1,11 +1,10 @@
-<?php while (have_posts()) : the_post(); ?>
-    <article <?php post_class(); ?>>
-        <header>
-            <h1 class="entry-title"><?php the_title(); ?></h1>
-        </header>
-        <div class="entry-content">
-            <p><a target="_blank" href="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]; ?>"><?php the_post_thumbnail( 'photo' ); ?></a></p>
-            <?php the_content(); ?>
-        </div>
-    </article>
-<?php endwhile; ?>
+<?php the_post(); ?>
+<article <?php post_class(); ?>>
+    <header>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header>
+    <div class="entry-content">
+        <p><a target="_blank" href="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]; ?>"><?php the_post_thumbnail( 'photo' ); ?></a></p>
+        <?php the_content(); ?>
+    </div>
+</article>
